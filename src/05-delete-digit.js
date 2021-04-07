b8b45cb82843e107f444f9eb34fd217a9e290749
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-07 21:55:28 +0300

    feat: complite deleteDigit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,16 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const splitedNumber = n.toString().split('');
+  const resultNumbersArray = [];
+  for (let i = 0; i < splitedNumber.length; i++) {
+    const tempArray = [...splitedNumber];
+    tempArray.splice(i, 1);
+    resultNumbersArray.push(Number(tempArray.join('')));
+  }
+  resultNumbersArray.sort((a, b) => b - a);
+  return resultNumbersArray[0];
 }
 
 module.exports = deleteDigit;
