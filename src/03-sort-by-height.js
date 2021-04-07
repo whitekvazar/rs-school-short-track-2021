21b7ef15eca0eafbf6253ee63a7f65d0405e7655
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-07 18:06:55 +0300

    feat: complite sortByHeight function

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,20 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let sortedArray = arr;
+  const indexArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      indexArray.push(i);
+    }
+  }
+  sortedArray = sortedArray.sort((a, b) => a - b);
+  sortedArray.splice(0, indexArray.length);
+  for (let i = 0; i < indexArray.length; i++) {
+    sortedArray.splice(indexArray[i], 0, -1);
+  }
+  return sortedArray;
 }
 
 module.exports = sortByHeight;
