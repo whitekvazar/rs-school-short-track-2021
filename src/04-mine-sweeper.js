4aa188cb9c9af3ad7ed903f9030607008467b6d3
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-07 20:46:19 +0300

    feat: complite mineSweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,55 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const tempMatrix = matrix.map((item) => [...item]);
+  const resultMatrix = matrix.map((item) => [...item]);
+  for (let i = 0; i < tempMatrix.length; i++) {
+    for (let j = 0; j < tempMatrix[i].length; j++) {
+      resultMatrix[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < tempMatrix.length; i++) {
+    for (let j = 0; j < tempMatrix[i].length; j++) {
+      if (tempMatrix[i][j] === true) {
+        // top line (3 elements above a minesweeper)
+        if (i > 0 && j > 0) {
+          resultMatrix[i - 1][j - 1] = resultMatrix[i - 1][j - 1] + 1;
+        }
+        if (i > 0) {
+          resultMatrix[i - 1][j] = resultMatrix[i - 1][j] + 1;
+          if (j + 1 < tempMatrix[i].length) {
+            resultMatrix[i - 1][j + 1] = resultMatrix[i - 1][j + 1] + 1;
+          }
+        }
+        // middle line (2 elements on the right and left of a minesweeper)
+        if (j > 0) {
+          resultMatrix[i][j - 1] = resultMatrix[i][j - 1] + 1;
+        }
+        if (j + 1 < tempMatrix[i].length) {
+          resultMatrix[i][j + 1] = resultMatrix[i][j + 1] + 1;
+        }
+        // bottom line (3 elements below a minesweeper)
+        if (i + 1 < tempMatrix.length) {
+          if (j > 0) {
+            resultMatrix[i + 1][j - 1] = resultMatrix[i + 1][j - 1] + 1;
+          }
+          resultMatrix[i + 1][j] = resultMatrix[i + 1][j] + 1;
+          if (j + 1 < tempMatrix[i].length) {
+            resultMatrix[i + 1][j + 1] = resultMatrix[i + 1][j + 1] + 1;
+          }
+        }
+      }
+    }
+  }
+  for (let i = 0; i < tempMatrix.length; i++) {
+    for (let j = 0; j < tempMatrix[i].length; j++) {
+      if (tempMatrix[i][j] === true) {
+        resultMatrix[i][j] = 1;
+      }
+    }
+  }
+  return resultMatrix;
 }
 
 module.exports = minesweeper;
