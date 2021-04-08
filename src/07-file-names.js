2d97993006b75761c537995d57fcb79261231aa3
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-08 15:12:39 +0300

    feat: complite renameFiles function

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,30 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const inputArray = [...names];
+  const resultArray = [];
+  let flag = false;
+  let counter = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < resultArray.length; j++) {
+      if (inputArray[i] === resultArray[j]) {
+        flag = true;
+        counter++;
+      }
+      for (let k = 0; k < resultArray.length; k++) {
+        if (`${inputArray[i]}(${k})` === resultArray[j]) {
+          counter++;
+        }
+      }
+    }
+    const outputWord = flag ? `${inputArray[i]}(${counter})` : inputArray[i];
+    resultArray.push(outputWord);
+    counter = 0;
+    flag = false;
+  }
+  /* console.log(resultArray); */
+  return resultArray;
 }
 
 module.exports = renameFiles;
