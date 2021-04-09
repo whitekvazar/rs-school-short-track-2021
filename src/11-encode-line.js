786811cf0d624a3ef4db59287f87b7525a80ebe8
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-09 17:59:53 +0300

    feat: complite encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const inputArray = str.split('');
+  let counter = 0;
+  const resultArray = [];
+  let resultString = '';
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < inputArray.length; j++) {
+      if (inputArray[i] === inputArray[j]) {
+        counter++;
+      } else if (counter > 0 && inputArray[i] !== inputArray[j]) {
+        break;
+      }
+    }
+    if (resultArray.indexOf(`${counter}${inputArray[i]}`) === -1) {
+      if (counter > 1) {
+        resultArray.push(`${counter}${inputArray[i]}`);
+      } else {
+        resultArray.push(inputArray[i]);
+      }
+    }
+    counter = 0;
+  }
+  resultString = resultArray.join('');
+  return resultString;
 }
 
 module.exports = encodeLine;
