ea1c59cafe00c6fb0ccadd5d70f4d88189f0ba8e
Kirill Romanovskii <whitekvazar@gmail.com>
2021-04-09 15:48:00 +0300

    feat: complite getSumOfDigits function

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,14 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const inputNumber = n;
+  let sum = 0;
+  const arrayOfdigits = inputNumber.toString().split('');
+  arrayOfdigits.forEach((digit) => {
+    sum += +digit;
+  });
+  return (sum > 9) ? getSumOfDigits(sum) : sum;
 }
 
 module.exports = getSumOfDigits;
